dd5816832e5a9f1b5c943068c54fa873645aae1d
Zed Lopez <zed@zedlopez.org>
2023-05-22 00:54:27 -0700

    fixing index.html

--- docs/i7doc.js
+++ docs/i7doc.js
@@ -1,4 +1,4 @@
-function copyCode(text) { navigator.clipboard.writeText(text); }
-function pasteCode(text) { navigator.clipboard.writeText(text); }
+function pasteCode(text) { Navigator.clipboard.writeText(text); }
+function copyCode(text) { Navigator.clipboard.writeText(text); }
 
 window.addEventListener("load", function(e) { let el = document.getElementById('current_year'); el.innerHTML = new Date().getFullYear(); }, false);
